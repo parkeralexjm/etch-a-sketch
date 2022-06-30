a425cffe0a669907e7f8e02f83f6a0c4d94a8d0d
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-06-30 21:45:25 +0100

    Added button to create new screens
    Created a button at the top of the page to create a user selected size
    of new pixels to draw on, incomplete as the width of the screen is
    fixed at 4 squares wide and only extends in the vertical axis. pixels
    should be scaling in size to take up the width.

--- javascript.js
+++ javascript.js
@@ -4,14 +4,30 @@ for (let i = 1; i <= 16; i++) {
     const div = document.createElement('div');
     div.id = i;
     div.className = 'pixel';
-    
     container.appendChild(div);
 }
 
-const target = document.querySelectorAll('.pixel');
+var target = document.querySelectorAll('.pixel');
 
     for (var i = 0; i < target.length; i++ ) {
         target[i].addEventListener('mouseover', function() {
             this.className = 'black';
         })
     }
+function newScreen() {
+    var size = window.prompt("What size would you like your Etch-A-Sketch to be?");
+    container.innerHTML = "";
+    for (let i = 1; i <= size; i++) {
+        const div = document.createElement('div');
+        div.id = i;
+        div.className = 'pixel';
+        container.appendChild(div);
+    }
+    target = document.querySelectorAll('.pixel');
+
+    for (var i = 0; i < target.length; i++ ) {
+        target[i].addEventListener('mouseover', function() {
+            this.className = 'black';
+        })
+    }
+}
