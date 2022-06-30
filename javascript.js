886137df0ead326d10aadfbecbae85da71605407
parkeralexjm <parkeralexjm@hotmail.co.uk>
2022-06-30 21:04:37 +0100

    Added mouseover functionality
    Added class change on mouseover to each of the pixels that are in the
    field.

--- javascript.js
+++ javascript.js
@@ -4,12 +4,14 @@ for (let i = 1; i <= 16; i++) {
     const div = document.createElement('div');
     div.id = i;
     div.className = 'pixel';
+    
     container.appendChild(div);
 }
 
-const pixels = document.getElementsByClassName('pixel')
-pixels.onmouseover = function(){mouseOver()};
+const target = document.querySelectorAll('.pixel');
 
-function mouseOver() {
-    pixels.style.color = 'black';
-}
+    for (var i = 0; i < target.length; i++ ) {
+        target[i].addEventListener('mouseover', function() {
+            this.className = 'black';
+        })
+    }
